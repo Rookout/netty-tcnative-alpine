c37716b32bffcd48979c55ac486b708cdf70bda1
Ori Shavit <ori@rookout.com>
2018-11-14 19:27:39 +0200

    Rookout changes

--- compile.sh
+++ compile.sh
@@ -3,8 +3,10 @@
 git clone https://github.com/netty/netty-tcnative
 cd netty-tcnative
 git checkout tags/$NETTY_TCNATIVE_TAG
-mvn clean package
+mvn clean package -pl boringssl-static -am
 
-mv openssl-dynamic/target/*.jar /output/
-mv openssl-static/target/*.jar /output/
-mv boringssl-static/target/*.jar /output/
+cd /output/
+unzip /netty-tcnative/boringssl-static/target/netty-tcnative-boringssl-static-2.0.17.Final-linux-x86_64.jar META-INF/native/libnetty_tcnative.so
+zip -r librook-netty-tcnative-boringssl-static-2.0.17-Final.zip META-INF
+rm -rf META-INF
+
